ee81150e189c3377ef6a02a79caa34d4fbf0fc38
Awab M.Najim <44520076+Gandol-SD@users.noreply.github.com>
2022-11-07 11:09:00 +0200

    #16 - Seed Data in Tables

---
+++ HotelListing.API/Migrations/20221107090606_SeededCountriesAndHotels.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListing.API.Migrations
+{
+    public partial class SeededCountriesAndHotels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[,]
+                {
+                    { 1, "Sudan", "SD" },
+                    { 2, "Jamaica", "JM" },
+                    { 3, "Egypt", "EG" },
+                    { 4, "Rwanda", "RW" },
+                    { 5, "St.Lucia", "LC" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "JG47+JF2, Nile Street, Al Khurtum, Sudan", 1, "Corinthia", 4.4000000000000004 },
+                    { 2, "Goerge Town, Jamaica", 2, "Comfort Suits", 4.2000000000000002 },
+                    { 3, "Cairo, Egypt", 3, "Grand Palladium", 4.2999999999999998 },
+                    { 4, "VW7F+GG3, Mamin, St. Lucia", 5, "Jade Mountain Resort", 4.7000000000000002 },
+                    { 5, "KN 3 Ave, Kigali, Rwanda", 4, "Kigali Marriott Hotel", 4.5999999999999996 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
